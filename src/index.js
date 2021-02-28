f1890dd8750560bedb9c7fb9a452e9672d49e33e
Deepak Chaurasiya <72331440+deepak252@users.noreply.github.com>
2021-02-28 12:24:20 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
 //
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My Favourite Foods </h1>
+    <ul>
+      <li>Momos</li>
+      <li>Samosa</li>
+      <li>Burger</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
